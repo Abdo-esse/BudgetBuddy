2a3ea0956e69f78e90afc00b2e45c0ed78fb6303
abdelilah2005 <abdelilahessemlali@gmail.com>
2025-03-13 01:40:09 +0000

    add all file de model tags

---
+++ app/Http/Controllers/TagsController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Tags;
+use App\Http\Requests\StoreTagsRequest;
+use App\Http\Requests\UpdateTagsRequest;
+
+class TagsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreTagsRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreTagsRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Tags  $tags
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Tags $tags)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateTagsRequest  $request
+     * @param  \App\Models\Tags  $tags
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateTagsRequest $request, Tags $tags)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Tags  $tags
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Tags $tags)
+    {
+        //
+    }
+}
