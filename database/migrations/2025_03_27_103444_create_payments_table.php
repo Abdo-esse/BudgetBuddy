246db060a34154f513c8717779c2e4b75b43f6a7
abdelilah2005 <abdelilahessemlali@gmail.com>
2025-03-27 12:06:07 +0000

    Add collumn payments table

--- database/migrations/2025_03_27_103444_create_payments_table.php
+++ database/migrations/2025_03_27_103444_create_payments_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('payments', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('group_id')->constrained()->onDelete('cascade');
+            $table->foreignId('payer_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('receiver_id')->constrained('users')->onDelete('cascade');
+            $table->decimal('amount', 10, 2);
             $table->timestamps();
         });
     }
